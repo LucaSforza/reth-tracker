ae67dcbea5c8c4beb2939e13cff4981f772ef37b
LucaSforza <lucasforza1234@icloud.com>
2025-12-08 17:59:41 +0100

    exploring the blockchain

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,13 @@
+use std::process::exit;
 use std::str::FromStr;
 
+use alloy::eips::BlockId;
 use alloy::network::Network;
 use alloy::providers::Provider;
 use alloy::{primitives::Address, providers::ProviderBuilder, transports::http::reqwest::Url};
 use anyhow::Result;
-use chrono::{DateTime, NaiveDateTime, Utc};
+use chrono::{DateTime, Utc};
 use clap::{arg, command, Parser, Subcommand};
-use ethers::types::Res;
 use reth_lib::rocket_token_reth::RocketTokenRETH;
 
 use rusqlite::Connection;
@@ -98,6 +99,16 @@ enum Command {
 
     /// Lists the tracked addresses and their staking reward history.
     List,
+
+    Popolate {
+        /// The Ethereum address to add.
+        #[arg(short, long)]
+        address: String,
+        /// start
+        start_block_id: u128, // TODO: bigger number
+        /// end
+        end_block_id: u128,
+    },
 }
 
 /// Main CLI configuration.
@@ -187,6 +198,57 @@ where
         address_eth_balance,
         total_eth_balance,
     ));
+}
+
+/// Retrieves staking and balance values for a given Ethereum address at a specified block.
+///
+/// # Parameters
+/// - `provider`: A reference to the Ethereum provider.
+/// - `address`: The Ethereum address to query.
+/// - `address_reth`: The rETH contract address.
+/// - `block_numer`: the block number
+///
+/// # Returns
+/// A `Result` containing a tuple with:
+/// - rETH balance (f64),
+/// - rETH converted to ETH (f64),
+/// - ETH balance of the address (f64),
+/// - total ETH balance including converted rETH (f64).
+async fn get_values_at_block<F, N>(
+    provider: &F,
+    address: Address,
+    address_reth: Address,
+    block_number: u64,
+) -> Result<(f64, f64, f64, f64)>
+where
+    F: Provider<N>,
+    N: Network,
+{
+    let block_id = BlockId::from(block_number);
+    // ETH normale
+    let eth_balance = provider.get_balance(address).block_id(block_id).await?;
+
+    let reth = RocketTokenRETH::new(address_reth, provider);
+
+    let decimals = reth.decimals().block(block_id).call().await?;
+
+    let balance_reth = reth.balanceOf(address).block(block_id).call().await?;
+
+    let balance_eth = reth
+        .getEthValue(balance_reth)
+        .block(block_id)
+        .call()
+        .await?;
+
+    let total_eth = eth_balance + balance_eth;
+
+    // converti da U256 / Wei a f64
+    let balance_reth_f = balance_reth.to_string().parse::<f64>()? / 10f64.powi(decimals as i32);
+    let eth_balance_f = eth_balance.to_string().parse::<f64>()? / 1e18;
+    let balance_eth_f = balance_eth.to_string().parse::<f64>()? / 1e18;
+    let total_eth_f = total_eth.to_string().parse::<f64>()? / 1e18;
+
+    Ok((balance_reth_f, balance_eth_f, eth_balance_f, total_eth_f))
 }
 
 /// Inserts staking reward data into the database.
@@ -320,6 +382,54 @@ async fn main() -> Result<()> {
                     println!();
                 }
             }
+        }
+        Command::Popolate {
+            address,
+            start_block_id,
+            end_block_id,
+        } => {
+            use futures::{stream::FuturesUnordered, StreamExt};
+            use indicatif::ProgressBar;
+
+            if end_block_id < start_block_id {
+                eprintln!("[ERROR] end-block-id can not be less tha start-block-id");
+                exit(1);
+            }
+            const BLOCK_IN_ONE_DAY: u128 = 7200;
+            let mut block = start_block_id;
+            let bar = ProgressBar::new(
+                ((end_block_id - start_block_id) / BLOCK_IN_ONE_DAY)
+                    .try_into()
+                    .expect("Too many blocks, report this error in https://github.com/LucaSforza/reth-tracker"),
+            );
+            let mut tasks = FuturesUnordered::new();
+            while block <= end_block_id {
+                tasks.push(get_values_at_block(
+                    &provider,
+                    get_address_str(address.as_str())?,
+                    address_reth,
+                    block as u64, // TODO: please change
+                ));
+                block += BLOCK_IN_ONE_DAY;
+            }
+            let conn = get_db()?;
+            while let Some(res) = tasks.next().await {
+                match res {
+                    Ok((balance_reth, balance_eth, address_eth_balance, total_eth_balance)) => {
+                        insert_into_database(
+                            &conn,
+                            &address,
+                            balance_reth,
+                            balance_eth,
+                            address_eth_balance,
+                            total_eth_balance,
+                        )?;
+                    }
+                    Err(e) => eprintln!("[WARNING] Error at block {}: {:?}", block, e),
+                }
+                bar.inc(1);
+            }
+            bar.finish();
         }
     }
 
